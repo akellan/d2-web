d8cdcdbfe5550709eec9e44aaa2f9dddc18ac053
akellan <akella.n@gmail.com>
2020-06-25 00:55:58 +0300

    feat: show all runes list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./App.css";
 import { Runewords } from "./data/runewords";
 
 function App() {
+  const runelist = useMemo(
+    () =>
+      Array.from(
+        Runewords.reduce((previous, current) => {
+          current.runes.forEach((rune) => previous.add(rune));
+          return previous;
+        }, new Set<string>())
+      ).sort(),
+    []
+  );
+
   return (
     <div className="App">
       <div className="container pt-5 pb-5">
+        <div className="row h4 mb-5">
+          {runelist.map((rune) => (
+            <span className="badge badge-info m-1">{rune}</span>
+          ))}
+        </div>
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
           {Runewords.map((runeword, index) => (
             <div key={index} className="col">
-              <div className="card m-2">
-                <div className="card-header bg-primary text-light h5">
+              <div className="card mb-5">
+                <div className="card-header bg-light text-primary h4">
                   {runeword.name}
                 </div>
                 <div className="card-body">
